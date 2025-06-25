48dcf92319dc7e588e89a1706a2ce01bd5d4622d
devopsEngr <36155709+devopsEngr@users.noreply.github.com>
2025-06-25 19:16:50 +1000

    Added controller file

---
+++ springboot-application/src/main/java/com/example/helloserver/controller/HelloController.java
@@ -0,0 +1,12 @@
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("OK");
+    }
+}
