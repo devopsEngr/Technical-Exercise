93bc7e327703513970dc2d7848fee645a73a6a4b
devopsEngr <36155709+devopsEngr@users.noreply.github.com>
2025-06-25 19:31:57 +1000

    Added package

--- springboot-application/src/main/java/com/example/helloserver/controller/HelloController.java
+++ springboot-application/src/main/java/com/example/helloserver/controller/HelloController.java
@@ -1,3 +1,5 @@
+package com.example.helloserver.controller;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
